e04e830b5db1ec2de05730bc3aebd832a53daee5
StijnDeBrauwer <db_stijn@hotmail.com>
2021-05-16 18:03:56 +0200

    add font faces

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
 import {initCssVariables} from 'src/helpers/style.helpers';
+import 'src/assets/fonts/fonts.style.scss';
 
 import App from './App';
 
