525b7d8e70ef93d6c29753c481c7c612c6b094ad
StijnDeBrauwer <db_stijn@hotmail.com>
2021-05-20 21:51:46 +0200

    slicing projects + add favicon

--- src/assets/images/images.const.ts
+++ src/assets/images/images.const.ts
@@ -1,11 +1,17 @@
 import homeMobile from './home-mobile.png';
 import homeDesktop from './home-desktop.png';
+import NCSP from './ncsp.png';
+import BPrints from './bprint.png';
+import OP2C from './op2c.png';
 import me from './me.png';
 
 const Images = {
   homeMobile,
   homeDesktop,
   me,
+  NCSP,
+  BPrints,
+  OP2C,
 };
 
 export default Images;
--- src/components/sections/projects/projects.component.tsx
+++ src/components/sections/projects/projects.component.tsx
@@ -5,17 +5,61 @@ import {withSection} from 'src/hoc/withSection/withSection.hoc';
 import './projects.style.scss';
 
 const ProjectsSection = () => {
+  const navigateToUrl = (url: string) => {
+    window.open(url, '_blank');
+  };
+
   return (
     <section className={'projects'}>
       <h1>Projects</h1>
 
       <div className="projects__container">
-        <div className="container__item">NSCP</div>
-        <div className="container__item">B-prints</div>
-        <div className="container__item">Portfolio</div>
-        <div className="container__item">Table tennis PWA</div>
-        <div className="container__item">OP2C</div>
-        <div className="container__item"></div>
+        <div
+          className="container__item"
+          onClick={() => navigateToUrl('https://www.newcrownservice.com/home')}>
+          <div className="text dark">NCSP</div>
+          <div className="overlay">
+            <div className="text">NCSP</div>
+          </div>
+        </div>
+        <div
+          className="container__item"
+          onClick={() => navigateToUrl('https://www.b-prints.be/#/home')}>
+          <div className="text dark">B-Prints</div>
+          <div className="overlay">
+            <div className="text">B-Prints</div>
+          </div>
+        </div>
+        <div
+          className="container__item"
+          onClick={() =>
+            navigateToUrl('https://github.com/StijnDeBrauwer/PersonalWebsite')
+          }>
+          <div className="text dark">Portfolio</div>
+          <div className="overlay">
+            <div className="text">Portfolio</div>
+          </div>
+        </div>
+        <div
+          className="container__item"
+          onClick={() =>
+            navigateToUrl('https://github.com/StijnDeBrauwer/rooigem-cms')
+          }>
+          <div className="text dark">Table tennis PWA</div>
+          <div className="overlay">
+            <div className="text">Table Tennis PWA</div>
+          </div>
+        </div>
+        <div
+          className="container__item"
+          onClick={() =>
+            navigateToUrl('https://stijndebrauwer.github.io/op2c/#/home')
+          }>
+          <div className="text dark">OP2C</div>
+          <div className="overlay">
+            <div className="text">OP2C</div>
+          </div>
+        </div>
       </div>
     </section>
   );
